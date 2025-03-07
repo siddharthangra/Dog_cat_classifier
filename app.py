1000f0ed7371bac8f9f4457a25a512c43eafc20a
siddharthangra <siddharthangra@gmail.com>
2025-03-07 20:44:56 +0530

    changes

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     app.run(host="0.0.0.0", port=port)
